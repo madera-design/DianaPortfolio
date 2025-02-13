b53085363926cdbcdf4a3852608926ec425dd945
jose <joseantoniomaderag@gmail.com>
2025-02-13 16:50:06 -0600

    ajustes textos

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -28,21 +28,21 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-              Soy Lic. en Comunicación con + 4 años de experiencia en el ámbito digital e impreso. He participado en una amplia variedad de proyectos, incluyendo el diseño de identidades visuales, la creación de interfaces de usuario para aplicaciones web y móviles, y el desarrollo de campañas publicitarias.
+              Soy una diseñadora gráfica y UX/UI con más de 3 años de experiencia en la creación de prototipos, diseño web y gestión de proyectos. Licenciada en Comunicación, cuento con más de 4 años de experiencia en el ámbito digital e impreso, lo que me ha permitido desarrollar una visión integral y versátil en el campo del diseño
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-              Me caracterizo por ser responsable, proactiva y poseer excelentes habilidades de comunicación, lo que me permite trabajar de manera eficiente y organizada en equipo. A lo largo de mi carrera, he colaborado con equipos multidisciplinarios para desarrollar productos innovadores y centrados en el usuario.
+              Mi enfoque se centra en el diseño centrado en el usuario, especializándome en branding, diseño responsivo y la creación de interfaces de usuario para aplicaciones web y móviles. He participado en una amplia variedad de proyectos, desde el diseño de identidades visuales hasta el desarrollo de campañas publicitarias, siempre buscando ofrecer soluciones creativas y efectivas.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-              Mi enfoque se basa en una investigación exhaustiva del usuario, asegurando que cada diseño sea no solo estéticamente atractivo, sino también funcional y accesible. Mi pasión por el diseño y la creatividad me impulsa a seguir aprendiendo y mejorando constantemente, buscando siempre nuevas formas de resolver problemas y crear experiencias memorables para los usuarios.              </p>
+              Mi objetivo es contribuir en entornos dinámicos donde pueda aplicar mis habilidades para crear experiencias de usuario intuitivas y atractivas, al mismo tiempo que gestiono proyectos con eficiencia y atención al detalle. Busco oportunidades que me permitan seguir creciendo profesionalmente y aportar valor a través de un diseño innovador y funcional.         </p>
             </div>
           </li>
         </ul>
