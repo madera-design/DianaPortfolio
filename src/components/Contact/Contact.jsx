eb991c93f34fb68ca4186b0bc5dada7941136e25
jose <joseantoniomaderag@gmail.com>
2025-02-13 16:39:59 -0600

    ajustes iconos

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import styles from "./Contact.module.css";
-import { getImageUrl } from "../../utils";
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaBehance } from "react-icons/fa";
 
 export const Contact = () => {
   return (
@@ -12,15 +15,19 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
+          <MdEmail size={35}/>
           <a href="mailto:denriquezbx@gmail.com">denriquezbx@gmail.com</a>
         </li>
         <li className={styles.link}>
+          <FaLinkedin size={35} />
           <a href="https://www.linkedin.com/in/dianaenriquezb/">dianaenriquezb</a>
         </li>
         <li className={styles.link}>
+          <FaWhatsapp size={35} />
           <a href="https://wa.me/5213121023117">+ 52 1 312 102 3117</a>     
         </li>
         <li className={styles.link}>
+          <FaBehance size={35} />
           <a target="_blank" href="https://www.behance.net/daeinez">Behance</a>
         </li>
       </ul>
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -4,6 +4,7 @@ import styles from "./Experience.module.css";
 import skills from "../../data/en/skills.json";
 import history from "../../data/en/history.json";
 import { getImageUrl } from "../../utils";
+import { MdOutlineWorkOutline } from "react-icons/md";
 
 export const Experience = () => {
   return (
@@ -26,8 +27,7 @@ export const Experience = () => {
           {history.map((historyItem, id) => {
             return (
               <li key={id} className={styles.historyItem}>
-                <svg className={styles.svgIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                  <path d="M48 0C21.5 0 0 21.5 0 48L0 464c0 26.5 21.5 48 48 48l96 0 0-80c0-26.5 21.5-48 48-48s48 21.5 48 48l0 80 96 0c26.5 0 48-21.5 48-48l0-416c0-26.5-21.5-48-48-48L48 0zM64 240c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zm112-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zM80 96l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zM272 96l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16z"/></svg>
+                <MdOutlineWorkOutline size={50}/>
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
